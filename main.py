a1eb9c7a860622d69ce9ba821c969055dc7220df
renxian <xian1995106@gmail.com>
2020-06-11 16:13:06 +0200

    von renxian

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ try:
     file_name = sys.argv[1]
     # print(file_name)
 
-    with open(file_name) as file_obj:
+    with open(file_name, 'r') as file_obj:
 
-        fileContent = file_obj.read()
-        print(fileContent)
+        lines = file_obj.readlines()[1:]  # skip the first line
+        list1 = []
+        for line in lines:
+            list1.append(line)
+
+        for i in list1:
+            i.strip()
+
 except FileNotFoundError:
     print('File does not exists: '+file_name)
     sys.exit(-1)
